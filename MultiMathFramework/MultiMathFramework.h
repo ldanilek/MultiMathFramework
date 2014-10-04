f73e4d5c330204fa73d7787cb036d4f9a99034f9
Lee Danilek <Lee@lees-air.wireless.yale.internal>
2014-10-04 19:45:26 -0400

    Initial Commit

---
+++ MultiMathFramework/MultiMathFramework.h
@@ -0,0 +1,19 @@
+//
+//  MultiMathFramework.h
+//  MultiMathFramework
+//
+//  Created by Lee Danilek on 10/4/14.
+//  Copyright (c) 2014 ShipShape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiMathFramework.
+FOUNDATION_EXPORT double MultiMathFrameworkVersionNumber;
+
+//! Project version string for MultiMathFramework.
+FOUNDATION_EXPORT const unsigned char MultiMathFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiMathFramework/PublicHeader.h>
+
+
